8603106ad9512b5702670605a93cc97b4d1b61c2
Almendra ivan ariel <almendraivan210814@gmail.com>
2020-06-29 15:49:56 -0300

    esto es una explicacion

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-//dentro de este carpeta va hacer un repositorio local
+// git init --> es una sola vez dentro de la carpeta 
+//git add . ---> guardamos todos los archivos en la cual isimos cambios otra dorma de hacer es ---> git add nombre.js
+//git commit -m explicacion_de_que_modificamos  ---> este es para agregar una explicacion de lo que guardamo
+
+//git status ----> es para saber el estado del archivo.
